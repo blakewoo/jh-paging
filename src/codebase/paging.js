6e7c2b1fbe74894550b687b7616295f7a7c7e60d
blakewoo <blakewoo0819@gmail.com>
2023-09-25 22:44:40 +0900

    Init codebase & example

---
+++ src/codebase/paging.js
@@ -0,0 +1,23 @@
+const JH_paging = (function () {
+
+    function JH_paging (CONTAINER,TOTAL_COUNT,CURRENT_COUNT) {
+        this.CONTAINER = CONTAINER
+        this.TOTAL_COUNT = TOTAL_COUNT
+        this.CURRENT_COUNT = CURRENT_COUNT;
+        this.paint()
+    }
+
+    JH_paging.prototype.paint = function () {
+        let container = this.CONTAINER
+    }
+
+    JH_paging.prototype.numberClick = function () {
+
+    }
+
+    JH_paging.prototype.arrowClick = function () {
+
+    }
+
+    return JH_paging
+}())
